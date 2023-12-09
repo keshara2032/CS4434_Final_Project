c0bed3d06a1b7ac1c3bdbeec4bd5ffa522102053
Uday Kiran <rfn3ua@virginia.edu>
2023-12-09 09:18:24 -0500

    Cold Standby support.

--- network_handler/receiver.py
+++ network_handler/receiver.py
@@ -2,23 +2,107 @@ import socket
 import time
 import pickle
 
-
-# code to control dobot
-
 # Set the source and target UDP ports
 relay_host = '127.0.0.1'  # Change to your relay host
 relay_port = 54321  # Change to your relay port
 
+sender_host = '127.0.0.1'  # Change to your relay host
+sender_port = 12346        # Change to your relay port
 
 relay_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 relay_socket.bind((relay_host, relay_port))
 
+start_received = False
+stop_received = False
+last_packet_time = time.time()
+last_received_data = -1  # Keep track of the last received data value
+
+def check_delay(last_packet_time, last_received_data):
+    # Check for delay and send delay packet to sender
+    print(f">>> {start_received} - {stop_received} - {(time.time() - last_packet_time)}")
+    if start_received and not stop_received and (time.time() - last_packet_time) > 0.05:  # 10ms delay
+        delay_packet = {'delay': last_received_data}
+        relay_socket.sendto(pickle.dumps(delay_packet), (sender_host, sender_port))
+        print("Delay detected. Sending delay packet to sender.")
 
 while True:
     data, addr = relay_socket.recvfrom(1024)  # Adjust buffer size as needed
     deserialized_packet = pickle.loads(data)
     
-    print(f"Received data from {addr}:")
-    for attr, value in vars(deserialized_packet).items():
-        print(f"{attr}: {value}")
+    if 'start' in deserialized_packet:
+        start_received = True
+        last_packet_time = time.time()
+        print(f"Received start from {addr}:")
+        last_received_data = -1  # Reset last_received_data for non-'data' packets
+    elif 'stop' in deserialized_packet:
+        stop_received = True
+        print(f"Received stop from {addr}:")
+        last_received_data = 0  # Reset last_received_data for non-'data' packets
+    elif 'data' in deserialized_packet:
+        if deserialized_packet['data'] > last_received_data:
+            check_delay(last_packet_time, last_received_data)
+            last_received_data = deserialized_packet['data']
+            last_packet_time = time.time()
+            print(f"Received data from {addr}: {last_received_data}   {last_packet_time}")
+    else:
+        # Handle regular data packet
+        print(f"Received data from {addr}:")
+        for attr, value in vars(deserialized_packet).items():
+            print(f"{attr}: {value}")
+        
+        last_received_data = 0  # Reset last_received_data for non-'data' packets
+        last_packet_time = time.time()
+
+
+
+# import socket
+# import time
+# import pickle
+
+# # Set the source and target UDP ports
+# relay_host = '127.0.0.1'  # Change to your relay host
+# relay_port = 54321  # Change to your relay port
+
+
+# sender_host = '127.0.0.1'  # Change to your relay host
+# sender_port = 12346        # Change to your relay port
+
+# relay_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# relay_socket.bind((relay_host, relay_port))
+
+# start_received = False
+# stop_received = False
+# last_packet_time = time.time()
+
+# def check_delay(last_packet_time):
+#     # Check for delay and send delay packet to sender
+#     print(f">>> {start_received} - {stop_received} - {(time.time() - last_packet_time)}")
+#     if start_received and not stop_received and (time.time() - last_packet_time) > 0.02:  # 10ms delay
+#         delay_packet = {'delay': True}
+#         relay_socket.sendto(pickle.dumps(delay_packet), (sender_host, sender_port))
+#         print("Delay detected. Sending delay packet to sender.")
+
+# while True:
+#     data, addr = relay_socket.recvfrom(1024)  # Adjust buffer size as needed
+#     deserialized_packet = pickle.loads(data)
     
+#     if 'start' in deserialized_packet:
+#         start_received = True
+#         last_packet_time = time.time()
+#         print(f"Received start from {addr}:")
+#     elif 'stop' in deserialized_packet:
+#         stop_received = True
+#         print(f"Received stop from {addr}:")
+#     elif 'data' in deserialized_packet:
+#         check_delay(last_packet_time)
+#         last_packet_time = time.time()
+#         print(f"Received data from {addr}: {deserialized_packet['data']}   {last_packet_time}")
+#     else:
+#         # Handle regular data packet
+#         print(f"Received data from {addr}:")
+#         for attr, value in vars(deserialized_packet).items():
+#             print(f"{attr}: {value}")
+        
+#         last_packet_time = time.time()
+
+ 
--- network_handler/sender.py
+++ network_handler/sender.py
@@ -1,35 +1,90 @@
 import socket
 import time
-from utils import precise_ms_delay, Packet
-import pickle
 import json
-    
-relay_host = '127.0.0.1'  # Change to your target host
-relay_port = 12345  # Change to your relay port
+import pickle
+from utils import precise_ms_delay, Packet
+import threading
 
-relay_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-delay_ms = 10
-
-# Sample object to send
-data_to_send = {'sequence': 'value'}  # Replace this with the object you want to send
-
+cold_stand_by_on = False
 num_packets = 0
 
-while True:
-    
-    if(num_packets == 1000):break
-    
-    # Convert the object to a JSON string
-    data_to_send['sequence'] = num_packets
-    # serialized_packet = json.dumps(data_to_send)
-    serialized_packet = f'{num_packets}'
+def sender_thread():
+    global cold_stand_by_on
+    relay_host = '127.0.0.1'  # Change to your relay host
+    relay_port = 12345  # Change to your relay port
+    relay_port_pri = 54321  # Change to your relay port
 
-    # data = bytes(data, 'utf-8')  # Encoding the string to bytes using utf-8
-    relay_socket.sendto(serialized_packet.encode('utf-8'), (relay_host, relay_port))
+    relay_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    relay_socket.setblocking(False)  # Set the socket to non-blocking mode
 
-    precise_ms_delay(delay_ms)   
-    
-    num_packets += 1
-     
-    # time.sleep(0.001) not accurate 
+    # Sample object to send
+    data_to_send = {'sequence': 'value'}  # Replace this with the object you want to send
+
+    # Send start packet
+    start_packet = {'start': True}
+    relay_socket.sendto(pickle.dumps(start_packet), (relay_host, relay_port))
+
+    num_packets = 0
+    total_packets = 1000  # Set the total number of packets you want to send
+
+    delay_ms = 10
+
+    while num_packets < total_packets:
+        # Convert the object to a JSON string
+        # data_to_send['sequence'] = num_packets
+        # serialized_packet = json.dumps(data_to_send)
+        # serialized_packet = f'{num_packets}'
+        data_packet = {'data': num_packets}
+
+        # relay_socket.sendto(serialized_packet.encode('utf-8'), (relay_host, relay_port))
+        if cold_stand_by_on:
+            relay_socket.sendto(pickle.dumps(data_packet), (relay_host, relay_port_pri))
+        else:
+            relay_socket.sendto(pickle.dumps(data_packet), (relay_host, relay_port))
+
+        precise_ms_delay(delay_ms) 
+        num_packets += 1
+
+    # Send stop packet
+    stop_packet = {'stop': True}
+    relay_socket.sendto(pickle.dumps(stop_packet), (relay_host, relay_port))
+
+    # Close the socket
+    relay_socket.close()
+
+def delay_detection_thread():
+    global cold_stand_by_on
+    relay_host = '127.0.0.1'  # Change to your sender host
+    relay_port =  12346       # Change to your sender port
+
+    relay_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    relay_socket.bind((relay_host, relay_port))
+
+    while True:
+        try:
+            data, addr = relay_socket.recvfrom(1024)  # Non-blocking
+            deserialized_packet = pickle.loads(data)
+
+            if 'delay' in deserialized_packet:
+                print(">> Delay detected. Adjusting accordingly.")
+                print(">> Cold stand by on")
+                cold_stand_by_on = True
+                num_packets = deserialized_packet['delay']
+                # Add your logic to handle the delay
+        except socket.error as e:
+            if e.errno == 35:  # Resource temporarily unavailable (non-blocking)
+                time.sleep(0.01)  # Adjust sleep time as needed
+            else:
+                raise  # Propagate other errors
+
+# Create and start the threads
+sender_thread = threading.Thread(target=sender_thread)
+delay_detection_thread = threading.Thread(target=delay_detection_thread)
+
+sender_thread.start()
+delay_detection_thread.start()
+
+# Wait for the sender thread to finish
+sender_thread.join()
+
+# The delay detection thread is not joined, as it runs indefinitely until manually stopped
